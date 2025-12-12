5c66a8268b9cf316b7248f08cd6a3355998ad659
vannes2 <vanesvernando72@gmail.com>
2025-12-12 19:16:13 +0700

    DNS lokal tersedia

--- app.py
+++ app.py
@@ -760,5 +760,11 @@ def admin_download_file():
 
 
 if __name__ == "__main__":
-    with app.app_context(): db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(
+        debug=True, 
+        host='0.0.0.0', 
+        port=443, 
+        ssl_context=('spk-anp.com+2.pem', 'spk-anp.com+2-key.pem')
+    )
